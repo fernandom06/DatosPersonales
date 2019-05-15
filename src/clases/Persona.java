f9be5621c93c804874dc67b7366126a985708d0c
Your Name <you@example.com>
2019-05-15 19:29:31 +0200

    commit

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -15,44 +15,81 @@ public class Persona implements Comparable<Persona>{
     private String apellido;
     private Integer id;
     
-    
+    /**
+     *
+     * @param nombre
+     * @param apellido
+     * @param id
+     * @param estatura
+     */
     public Persona(String nombre, String apellido, Integer id, Float estatura) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.id = id;
     }
     
-
+    /**
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getApellido() {
         return apellido;
     }
 
+    /**
+     *
+     * @param apellido
+     */
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
 
+    /**
+     *
+     * @return
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id
+     */
     public void setId(Integer id) {
         this.id = id;
     }
     
-    
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         return "Persona{" + "nombre=" + getNombre() + ", apellido=" + getApellido() + ", id=" + getId() + '}'+"\n";
     }
 
+    /**
+     *
+     * @param t
+     * @return
+     */
     @Override
     public int compareTo(Persona t) {
         
